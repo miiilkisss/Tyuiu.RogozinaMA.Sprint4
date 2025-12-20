71f3f58dad8d806bf8cdc8b2a00ac17adece2c8f
Vlad <Vlad@VLAD_X>
2025-12-20 19:18:06 +0500

    sdasdsadsa

---
+++ Tyuiu.RogozinaMA.Sprint4.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,33 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RogozinaMA.Sprint4.Task7.V11.Lib
+{
+    public class DataService : ISprint4Task7V11
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            //Дана строка из одноразрядных цифр "56789012". Преобразуйте ее в матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице.
+            int[,] matrix = new int[n, m];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    index++;
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.RogozinaMA.Sprint4.Task7.V11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.RogozinaMA.Sprint4.Task7.V11.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint4.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            // Дана строка из одноразрядных цифр "56789012". Преобразуйте ее в матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице.
+            string value = "56789012";
+            int n = 4;
+            int m = 2;
+            int result = ds.Calculate(n, m, value);
+            Console.WriteLine(result);
+            Console.WriteLine(
+                result == 4 ? "Test passed." : "Test failed."
+            );
+            Console.WriteLine(
+                "Press any key to continue..."
+            );
+            Console.ReadKey();
+
+
+        }
+    }
+}
