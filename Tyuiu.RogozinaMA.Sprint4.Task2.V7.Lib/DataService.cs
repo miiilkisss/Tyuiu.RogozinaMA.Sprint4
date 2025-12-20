933cf1e08d2d76418cbf1c4e4e3ef97a5a4b15e7
Vlad <Vlad@VLAD_X>
2025-12-20 18:37:02 +0500

    Task,2

---
+++ Tyuiu.RogozinaMA.Sprint4.Task2.V7.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RogozinaMA.Sprint4.Task2.V7.Lib
+{
+    public class DataService : ISprint4Task2V7
+    {
+        public int Calculate(int[] array)
+        {
+            //Дан одномерный целочисленный массив на 12 элементов заполненный случайными в диапазоне от 4 до 9 подсчитать сумму четных элементов массива.
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+
+        }
+    }
+}
---
+++ Tyuiu.RogozinaMA.Sprint4.Task2.V7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+using Tyuiu.RogozinaMA.Sprint4.Task2.V7.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint4.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            //Дан одномерный целочисленный массив на 12 элементов заполненный случайными в диапазоне от 4 до 9 подсчитать сумму четных элементов массива.
+            int[] array = new int[12];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(4, 10);
+            }
+            Console.WriteLine("Массив: " + string.Join(", ", array));
+            int result = ds.Calculate(array);
+            Console.WriteLine("Сумма четных элементов массива: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
