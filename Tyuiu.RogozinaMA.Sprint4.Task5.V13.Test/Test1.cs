8c8161218b08c77230007dad25f7382673ff2141
Vlad <Vlad@VLAD_X>
2025-12-20 19:06:43 +0500

    Tsasd

---
+++ Tyuiu.RogozinaMA.Sprint4.Task5.V13.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.RogozinaMA.Sprint4.Task5.V13.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RogozinaMA.Sprint4.Task5.V13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+using Tyuiu.RogozinaMA.Sprint4.Task5.V13.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint4.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[,]
+            {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+            };
+            int[,] result = ds.Calculate(matrix);
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
